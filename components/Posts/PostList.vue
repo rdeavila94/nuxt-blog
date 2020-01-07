260416aad95135c8202af320bc9ba036063bf5be
rdeavila <richard.deavila@mobileforming.com>
2020-01-07 14:16:53 -0800

    Completed admin page for the posts

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="featured-posts">
+    <PostPreview
+      id="1"
+      thumbnail="https://i.ytimg.com/vi/uz5w5KU2HJ0/maxresdefault.jpg"
+      title="Hello There!"
+      previewText="This is my first post"
+      :is-admin="isAdmin"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://i.ytimg.com/vi/uz5w5KU2HJ0/maxresdefault.jpg"
+      title="Hello There - The second time!"
+      previewText="This is my second post"
+      :is-admin="isAdmin"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://i.ytimg.com/vi/uz5w5KU2HJ0/maxresdefault.jpg"
+      title="Hi There"
+      previewText="This is my third post"
+      :is-admin="isAdmin"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+  name: "PostList",
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Maximilian",
+        title: "My Awesome Post",
+        content: "Super amazing",
+        thumbnailLink: "https://i.ytimg.com/vi/uz5w5KU2HJ0/maxresdefault.jpg"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
