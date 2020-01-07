70453719c1f014e2c31905314f57676db753874d
rdeavila <richard.deavila@mobileforming.com>
2020-01-07 14:41:17 -0800

    Created a new admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+
+export default {};
+</script>
+
+<style>
+html {
+  font-family: "Open Sans", sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
